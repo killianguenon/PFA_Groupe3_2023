b70e2d6a50abed263107d19efba37d73a912d871
killianguenon <98550745+killianguenon@users.noreply.github.com>
2023-04-28 18:25:48 +0200

    integration Du Scrolling Des bouttons

---
+++ PFA_2D/Assets/ParameterAll.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParameterAll:MonoBehaviour
+{
+    public float visualActionStrength = 1;
+}
---
+++ PFA_2D/Assets/VisualAction.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualAction : MonoBehaviour
+{
+    [SerializeField]
+    GameObject targetButtonAction;
+    ParameterAll ParameterAll;
+    public Function function;
+
+    Vector2 vector2Strength, vector2PositionStart;
+    float strength;
+
+    private void Start()
+    {
+        vector2PositionStart = transform.position - targetButtonAction.transform.position;
+        ParameterAll = FindObjectOfType<ParameterAll>();
+        function = FindObjectOfType<Function>();
+    }
+
+    void Update()
+    {
+        strength = ParameterAll.visualActionStrength;
+        vector2Strength = new Vector2(strength, 0);
+
+        function.LookAt2D(gameObject, targetButtonAction);
+        function.ScroolAndZoom2D(gameObject, targetButtonAction, vector2PositionStart);
+
+        this.GetComponent<Rigidbody2D>().AddRelativeForce(vector2Strength);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == targetButtonAction.gameObject)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
